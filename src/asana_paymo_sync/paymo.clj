c7ce55f98e6d0e0f9dbad972de0b62d6bf67b794
Philipp Küng <git@philippkueng.ch>
2015-01-09 18:58:54 +0100

    retrieving projects from paymo; adding tasklists and tasks to paymo

--- src/asana_paymo_sync/paymo.clj
+++ src/asana_paymo_sync/paymo.clj
@@ -14,8 +14,43 @@
       (:body)
       (parse-string true)))
 
+(defn paymo-post
+  "Wrapper to include the authentication header for POST calls"
+  [url data]
+  (client/post (str (:api-url config/paymo)
+                    url)
+               {:basic-auth [(:username config/paymo) (:password config/paymo)]
+                :accept :json
+                :form-params data
+                :content-type :json}))
+
 (defn clients
   "Retrieve clients from paymo"
   []
   (paymo-get "clients"))
+
+(defn projects
+  "Retrieve projects from paymo"
+  []
+  (paymo-get "projects"))
+
+(defn project
+  "Retrieves a project with its tasklists and tasks from paymo"
+  [project-id]
+  (paymo-get (str "projects/"
+                  project-id
+                  "?include=tasklists,tasks")))
+
+(defn create-tasklist
+  [name project-id]
+  (paymo-post "tasklists"
+              {:name name
+               :project_id project-id}))
+
+(defn create-task
+  [name tasklist-id project-id]
+  (paymo-post "tasks"
+              {:name name
+               :tasklist_id tasklist-id
+               :project_id project-id}))
 
