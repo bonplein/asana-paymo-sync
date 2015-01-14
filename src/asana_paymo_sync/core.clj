ae9b255694cfcb580ea38dbbc2b2ba958b10d450
Philipp Küng <git@philippkueng.ch>
2015-01-14 01:42:15 +0100

    define a main function which provider a simple CLI interface to return projects for both providers
    a functionality provided to help with the project mapping required in config.clj

--- src/asana_paymo_sync/core.clj
+++ src/asana_paymo_sync/core.clj
@@ -1,6 +1,24 @@
-(ns asana-paymo-sync.core)
+(ns asana-paymo-sync.core
+  (:require [asana-paymo-sync.asana :as asana]
+            [asana-paymo-sync.paymo :as paymo]
+            [asana-paymo-sync.config :as config]
+            [clojure.pprint :as pprint]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  "Command-line entry point."
+  [& args]
+  (let [provider (first args)
+        resource (nth args 1)]
+    (-> (case provider
+          "asana" (case resource
+                    "projects" (asana/projects)
+                    "Please provide a resource.")
+          "paymo" (case resource
+                    "projects" (->> (paymo/projects)
+                                   (:projects)
+                                   (map #(into (sorted-map)
+                                               (select-keys % [:id :name]))))
+                    "Please provide a resource."))
+        (pprint/pprint)
+        (println))))
+
