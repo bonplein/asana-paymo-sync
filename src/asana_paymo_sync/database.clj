5d37bbda74fb480bf3a3b5edeca64109d3d8b5b4
Philipp Küng <git@philippkueng.ch>
2015-01-24 14:58:58 +0100

    fix: converting the dynamo number format to long #24898125395256

--- src/asana_paymo_sync/database.clj
+++ src/asana_paymo_sync/database.clj
@@ -2,13 +2,21 @@
   (:require [taoensso.faraday :as far]
             [asana-paymo-sync.config :as config]))
 
+(defn longify
+  "Converts the number into a long, leaves nil"
+  [value]
+  (if (nil? value)
+    nil
+    (long value)))
+
 (defn get-tuple
   "Get the correlating Paymo key"
   [asana-key]
   (-> (far/get-item config/dynamodb
                     (:table config/dynamodb)
                     {:asana asana-key})
-      :paymo))
+      :paymo
+      longify))
 
 (defn set-tuple
   "Set the Asana and correlating Paymo key"
