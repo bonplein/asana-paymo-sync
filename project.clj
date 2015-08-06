3cb302b1a1d3a7f91bddea302163ed983042936d
Philipp Küng <git@philippkueng.ch>
2015-08-06 23:16:01 +0200

    defining a minimum version for leiningen

--- project.clj
+++ project.clj
@@ -11,5 +11,6 @@
   :plugins [[lein-environ "1.0.0"]]
   :profiles {:dev {:dependencies [[clj-http-fake "1.0.1"]]}}
   :uberjar-name "asana-paymo-sync-standalone.jar"
+  :min-lein-version "2.5.0"
   :main asana-paymo-sync.core
   :aot [asana-paymo-sync.core])
